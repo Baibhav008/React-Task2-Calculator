0a0c4f1e439ca1c06963250421935b549f88240e
Baibhav <baibhavk6@gmail.com>
2023-10-10 23:00:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Calc from './components/Calc';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background:" linear-gradient(177deg, rgba(0,0,0,1) 0%, rgba(9,79,121,1) 39%, rgba(11,149,177,1) 100%)"}}>
+      
+      <Calc/>
     </div>
   );
 }
